46ce742f894aaaccd86e39053e9cb713d443c998
이아름 <xox_lol@naver.com>
2024-12-26 14:55:11 +0900

    feat: 5. 동적 쿼리에 대한 이해

---
+++ src/main/java/com/example/demo/config/QueryDslConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Spring이 자동으로 JPAQueryFactory를 만들어 관리하도록 설정
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
