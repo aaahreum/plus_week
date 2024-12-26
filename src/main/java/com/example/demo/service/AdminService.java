e372ff9a152753942a6cc736d9079ef7b5fb564b
이아름 <xox_lol@naver.com>
2024-12-26 13:20:10 +0900

    feat: 4. DB 접근 최소화

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,21 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        // userIds 리스트에 해당하는 사용자 데이터를 한 번의 쿼리로 가져오고 한 번의 update 쿼리로 업데이트한다.
+        List<User> users = userRepository.findAllById(userIds);
 
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
+        if(users.isEmpty()){
+            throw new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다.");
         }
+
+        userRepository.updatePendingStatus(userIds);
+
+//        for (Long userId : userIds) {
+//            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+//
+//            user.updateStatusToBlocked();
+//
+//            userRepository.save(user);
+//        }
     }
 }
